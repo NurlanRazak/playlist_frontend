07d1afe975472d92fa3bae9de5a1b278f95a2c8c
Nurlan <nurlan.adiluly@gmail.com>
2020-09-07 23:19:59 +0600

    pagination style

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -3,20 +3,53 @@ import React from "react";
 class Playlist extends React.Component
 {
     state = {
-        performers: []
+        performers: [],
+        total: null,
+        per_page: null,
+        current_page: null,
+        page: null
     };
 
     async componentDidMount() {
-        const url = "http://playlist.local.com/api/performer";
+        this.getPerformersWithPage(1)
+    }
+
+    getPerformersWithPage = async pageNumber => {
+        const url = `http://playlist.local.com/api/performer?page=${pageNumber}`;
 
         // const url = "http://127.0.0.1:8000/api/performer";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ performers: data.data })
+        this.setState({
+            performers: data.data,
+            total: data.total,
+            per_page: data.per_page,
+            current_page: data.current_page
+        });
     }
+
     render() {
         if (!this.state.performers) {
             return <div> Performers not found</div>;
+        }
+
+        let renderPageNumbers;
+
+        var pageNumbers = [];
+        if (!this.state.total != null) {
+            for (let i=1; i <= Math.ceil(this.state.total / this.state.per_page); i++) {
+                pageNumbers.push(i);
+            }
+            console.log(pageNumbers);
+            renderPageNumbers = pageNumbers.map(number => {
+                let classes = this.state.current_page === number ? 'active' : undefined;
+
+                if ((number >= this.state.current_page - 2 && number <= this.state.current_page + 2)) {
+                    return (
+                      <span key={number} className={classes} onClick={() => this.getPerformersWithPage(number)}>{number}</span>
+                    );
+                  }
+            });
         }
 
         return (
@@ -40,6 +73,12 @@ class Playlist extends React.Component
                         ))}
                     </tbody>
                 </table>
+
+                <div className="pagination">
+                    <span onClick={() => this.getPerformersWithPage(1)}>&lt;</span>
+                        {renderPageNumbers}
+                    <span onClick={() => this.getPerformersWithPage(1)}>&gt;</span>
+                </div>
             </div>
         );
     }
